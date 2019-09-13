648afbdebc736f47c5014bbb4bae791f0708f424
nixberg <k.nixberg@gmail.com>
2019-09-13 21:58:31 +0200

    Partially unroll the permutation

--- Sources/Gimli/Gimli.swift
+++ Sources/Gimli/Gimli.swift
@@ -7,6 +7,7 @@ public struct Gimli {
     
     public subscript(index: Int) -> UInt8 {
         get {
+            precondition(0 <= index && index < 48)
             return withUnsafePointer(to: s) {
                 $0.withMemoryRebound(to: UInt8.self, capacity: 48) {
                     $0[index]
@@ -32,31 +33,45 @@ public struct Gimli {
     }
     
     public mutating func permute() {
-        for round in (UInt32(1)...24).reversed() {
-            let x = s.0.rotated(by: 24)
-            let y = s.1.rotated(by:  9)
-            let z = s.2
+        [0x9e377918 as UInt32, 0x9e377914, 0x9e377910, 0x9e37790c, 0x9e377908, 0x9e377904].forEach {
+            self.applySPBox()
+            self.smallSwap()
+            s.0.x ^= $0
             
-            s.2 = x ^ (z &<< 1) ^ ((y & z) &<< 2)
-            s.1 = y ^  x        ^ ((x | z) &<< 1)
-            s.0 = z ^  y        ^ ((x & y) &<< 3)
+            self.applySPBox()
             
-            switch round % 4 {
-            case 0:
-                s.0 = s.0[SIMD4<Int>(1, 0, 3, 2)]
-                s.0 ^= SIMD4<UInt32>(0x9e377900 ^ round, 0, 0, 0)
-            case 2:
-                s.0 = s.0[SIMD4<Int>(2, 3, 0, 1)]
-            default:
-                break
-            }
+            self.applySPBox()
+            self.bigSwap()
+            
+            self.applySPBox()
         }
+    }
+    
+    @inline(__always)
+    private mutating func applySPBox() {
+        let x = s.0.rotated(by: 24)
+        let y = s.1.rotated(by:  9)
+        let z = s.2
+        
+        s.2 = x ^ (z &<< 1) ^ ((y & z) &<< 2)
+        s.1 = y ^  x        ^ ((x | z) &<< 1)
+        s.0 = z ^  y        ^ ((x & y) &<< 3)
+    }
+    
+    @inline(__always)
+    private mutating func smallSwap() {
+        s.0 = s.0[SIMD4(1, 0, 3, 2)]
+    }
+    
+    @inline(__always)
+    private mutating func bigSwap() {
+        s.0 = s.0[SIMD4(2, 3, 0, 1)]
     }
 }
 
 extension SIMD4 where Scalar == UInt32 {
     @inline(__always)
-    func rotated(by n: UInt32) -> SIMD4<UInt32> {
+    func rotated(by n: UInt32) -> Self {
         (self &<< n) | (self &>> (32 &- n))
     }
 }
