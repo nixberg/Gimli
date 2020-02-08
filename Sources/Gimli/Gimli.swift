3fb7f700e9b2604eae1a3dec3c5fae7c27a77bad
nixberg <k.nixberg@gmail.com>
2020-02-08 16:17:38 +0100

    Refactor the permutation

--- Sources/Gimli/Gimli.swift
+++ Sources/Gimli/Gimli.swift
@@ -7,15 +7,16 @@ public struct Gimli {
     
     public subscript(index: Int) -> UInt8 {
         get {
-            precondition(0 <= index && index < 48)
+            precondition((0..<48).contains(index))
             return withUnsafePointer(to: s) {
                 $0.withMemoryRebound(to: UInt8.self, capacity: 48) {
                     $0[index]
                 }
             }
         }
+        // TODO: modify
         set {
-            precondition(0 <= index && index < 48)
+            precondition((0..<48).contains(index))
             withUnsafeMutablePointer(to: &s) {
                 $0.withMemoryRebound(to: UInt8.self, capacity: 48) {
                     $0[index] = newValue
@@ -25,46 +26,31 @@ public struct Gimli {
     }
     
     public mutating func permute() {
-        self.quadrupleRound(0x9e377918)
-        self.quadrupleRound(0x9e377914)
-        self.quadrupleRound(0x9e377910)
-        self.quadrupleRound(0x9e37790c)
-        self.quadrupleRound(0x9e377908)
-        self.quadrupleRound(0x9e377904)
-    }
-    
-    private mutating func quadrupleRound(_ constant: UInt32) {
-        self.applySPBox()
-        self.smallSwap()
-        s.0.x ^= constant
+        @inline(__always)
+        func applySPBox() {
+            let x = s.0.rotated(by: 24)
+            let y = s.1.rotated(by:  9)
+            let z = s.2
+            
+            s.2 = x ^ (z &<< 1) ^ ((y & z) &<< 2)
+            s.1 = y ^  x        ^ ((x | z) &<< 1)
+            s.0 = z ^  y        ^ ((x & y) &<< 3)
+        }
         
-        self.applySPBox()
+        func quadrupleRound(_ constant: UInt32) {
+            applySPBox()
+            s.0.smallSwap()
+            s.0.x ^= constant
+            
+            applySPBox()
+            
+            applySPBox()
+            s.0.bigSwap()
+            
+            applySPBox()
+        }
         
-        self.applySPBox()
-        self.bigSwap()
-        
-        self.applySPBox()
-    }
-    
-    @inline(__always)
-    private mutating func applySPBox() {
-        let x = s.0.rotated(by: 24)
-        let y = s.1.rotated(by:  9)
-        let z = s.2
-        
-        s.2 = x ^ (z &<< 1) ^ ((y & z) &<< 2)
-        s.1 = y ^  x        ^ ((x | z) &<< 1)
-        s.0 = z ^  y        ^ ((x & y) &<< 3)
-    }
-    
-    @inline(__always)
-    private mutating func smallSwap() {
-        s.0 = s.0[SIMD4(1, 0, 3, 2)]
-    }
-    
-    @inline(__always)
-    private mutating func bigSwap() {
-        s.0 = s.0[SIMD4(2, 3, 0, 1)]
+        [0x9e377918, 0x9e377914, 0x9e377910, 0x9e37790c, 0x9e377908, 0x9e377904].forEach(quadrupleRound)
     }
 }
 
@@ -72,5 +58,15 @@ fileprivate extension SIMD4 where Scalar == UInt32 {
     @inline(__always)
     func rotated(by n: UInt32) -> Self {
         (self &<< n) | (self &>> (32 &- n))
+    }
+    
+    @inline(__always)
+    mutating func smallSwap() {
+        self = self[SIMD4(1, 0, 3, 2)]
+    }
+    
+    @inline(__always)
+    mutating func bigSwap() {
+        self = self[SIMD4(2, 3, 0, 1)]
     }
 }
