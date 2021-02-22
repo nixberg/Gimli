832d080a12466c411e92ab517c5c46bcc388d6b9
nixberg <k.nixberg@gmail.com>
2021-02-22 13:34:07 +0100

    Remove unsafe code

--- Sources/Gimli/Gimli.swift
+++ Sources/Gimli/Gimli.swift
@@ -1,63 +1,98 @@
 public struct Gimli {
-    var s: (SIMD4<UInt32>, SIMD4<UInt32>, SIMD4<UInt32>)
+    var state: [UInt8]
     
     public init() {
-        s = (.zero, .zero, .zero)
+        state = .init(repeating: 0, count: 48)
     }
     
+    @inline(__always)
     public subscript(index: Int) -> UInt8 {
         get {
-            precondition((0..<48).contains(index))
-            return withUnsafePointer(to: s) {
-                $0.withMemoryRebound(to: UInt8.self, capacity: 48) {
-                    $0[index]
-                }
-            }
+            state[index]
         }
         // TODO: modify
         set {
-            precondition((0..<48).contains(index))
-            withUnsafeMutablePointer(to: &s) {
-                $0.withMemoryRebound(to: UInt8.self, capacity: 48) {
-                    $0[index] = newValue
-                }
-            }
+            state[index] = newValue
         }
     }
     
+    @inline(__always)
+    private func unpack() -> (SIMD4<UInt32>, SIMD4<UInt32>, SIMD4<UInt32>) {
+        var state = self.state[...]
+        let a = SIMD4(littleEndianBytes: state.prefix(16))
+        state = state.dropFirst(16)
+        let b = SIMD4(littleEndianBytes: state.prefix(16))
+        state = state.dropFirst(16)
+        let c = SIMD4(littleEndianBytes: state.prefix(16))
+        return (a, b, c)
+    }
+    
+    @inline(__always)
+    private mutating func pack(_ a: SIMD4<UInt32>, _ b: SIMD4<UInt32>, _ c: SIMD4<UInt32>) {
+        state.removeAll(keepingCapacity: true)
+        state.append(littleEndianBytesOf: a)
+        state.append(littleEndianBytesOf: b)
+        state.append(littleEndianBytesOf: c)
+    }
+    
     public mutating func permute() {
+        var (a, b, c) = self.unpack()
+        
         @inline(__always)
         func applySPBox() {
-            let x = s.0.rotated(by: 24)
-            let y = s.1.rotated(by:  9)
-            let z = s.2
+            let x = a.rotated(left: 24)
+            let y = b.rotated(left:  9)
+            let z = c
             
-            s.2 = x ^ (z &<< 1) ^ ((y & z) &<< 2)
-            s.1 = y ^  x        ^ ((x | z) &<< 1)
-            s.0 = z ^  y        ^ ((x & y) &<< 3)
+            c = x ^ (z &<< 1) ^ ((y & z) &<< 2)
+            b = y ^  x        ^ ((x | z) &<< 1)
+            a = z ^  y        ^ ((x & y) &<< 3)
         }
         
         func quadrupleRound(_ constant: UInt32) {
             applySPBox()
-            s.0.smallSwap()
-            s.0.x ^= constant
+            a.smallSwap()
+            a.x ^= constant
             
             applySPBox()
             
             applySPBox()
-            s.0.bigSwap()
+            a.bigSwap()
             
             applySPBox()
         }
         
-        [0x9e377918, 0x9e377914, 0x9e377910, 0x9e37790c, 0x9e377908, 0x9e377904].forEach(quadrupleRound)
+        quadrupleRound(0x9e377918)
+        quadrupleRound(0x9e377914)
+        quadrupleRound(0x9e377910)
+        quadrupleRound(0x9e37790c)
+        quadrupleRound(0x9e377908)
+        quadrupleRound(0x9e377904)
+        
+        self.pack(a, b, c)
     }
 }
 
 fileprivate extension SIMD4 where Scalar == UInt32 {
     @inline(__always)
-    func rotated(by n: UInt32) -> Self {
-        (self &<< n) | (self &>> (32 &- n))
+    init(littleEndianBytes bytes: ArraySlice<UInt8>) {
+        assert(bytes.count == Self.scalarCount * Scalar.bitWidth / 8)
+        
+        var bytes = bytes
+        let x = UInt32(littleEndianBytes: bytes.prefix(4))
+        bytes = bytes.dropFirst(4)
+        let y = UInt32(littleEndianBytes: bytes.prefix(4))
+        bytes = bytes.dropFirst(4)
+        let z = UInt32(littleEndianBytes: bytes.prefix(4))
+        bytes = bytes.dropFirst(4)
+        let w = UInt32(littleEndianBytes: bytes.prefix(4))
+        
+        self = .init(x, y, z, w)
+    }
+    
+    @inline(__always)
+    func rotated(left count: UInt32) -> Self {
+        (self &<< count) | (self &>> (32 - count))
     }
     
     @inline(__always)
@@ -68,5 +103,26 @@ fileprivate extension SIMD4 where Scalar == UInt32 {
     @inline(__always)
     mutating func bigSwap() {
         self = self[SIMD4(2, 3, 0, 1)]
+    }
+}
+
+fileprivate extension UInt32 {
+    @inline(__always)
+    init(littleEndianBytes bytes: ArraySlice<UInt8>) {
+        assert(bytes.count == Self.bitWidth / 8)
+        self = bytes
+            .reversed()
+            .reduce(0) { $0 << 8 | Self($1) }
+    }
+}
+
+fileprivate extension Array where Element == UInt8 {
+    @inline(__always)
+    mutating func append(littleEndianBytesOf x: SIMD4<UInt32>) {
+        for i in x.indices {
+            for count in stride(from: 0, to: 32, by: 8) {
+                self.append(UInt8(truncatingIfNeeded: x[i] >> count))
+            }
+        }
     }
 }
--- Tests/GimliTests/GimliTests.swift
+++ Tests/GimliTests/GimliTests.swift
@@ -1,27 +1,8 @@
 import XCTest
-@testable import Gimli
+import Gimli
 
 final class GimliTests: XCTestCase {
     func testGimli() {
-        var gimli = Gimli()
-        
-        for i in 0..<4 {
-            var j = UInt32(i)
-            gimli.s.0[i] = j &* j &* j &+ j &* 0x9e3779b9
-            j = UInt32(i + 4)
-            gimli.s.1[i] = j &* j &* j &+ j &* 0x9e3779b9
-            j = UInt32(i + 8)
-            gimli.s.2[i] = j &* j &* j &+ j &* 0x9e3779b9
-        }
-        
-        gimli.permute()
-        
-        XCTAssertEqual(gimli.s.0, [0xba11c85a, 0x91bad119, 0x380ce880, 0xd24c2c68])
-        XCTAssertEqual(gimli.s.1, [0x3eceffea, 0x277a921c, 0x4f73a0bd, 0xda5a9cd8])
-        XCTAssertEqual(gimli.s.2, [0x84b673f0, 0x34e52ff7, 0x9e2bef49, 0xf41bb8d6])
-    }
-    
-    func testGimliBytes() {
         var gimli = Gimli()
         
         for _ in 0..<384 {
