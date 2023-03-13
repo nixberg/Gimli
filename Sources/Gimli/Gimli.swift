3d3097b2cb0b05a588e49e8dd35535c6df5cb952
nixberg <310825+nixberg@users.noreply.github.com>
2023-03-13 14:50:09 +0100

    Remove noisy, untestable big-endian support

--- Sources/Gimli/Gimli.swift
+++ Sources/Gimli/Gimli.swift
@@ -6,10 +6,6 @@ public struct Gimli {
     public init() {}
     
     public mutating func permute() {
-        a = SIMD4(littleEndian: a)
-        b = SIMD4(littleEndian: b)
-        c = SIMD4(littleEndian: c)
-        
         for roundConstant: UInt32 in stride(from: 0x9e37_7918, to: 0x9e37_7900, by: -4) {
             self.applySPBox()
             a = a[1, 0, 3, 2]
@@ -22,10 +18,6 @@ public struct Gimli {
             
             self.applySPBox()
         }
-        
-        a = a.littleEndian
-        b = b.littleEndian
-        c = c.littleEndian
     }
     
     @inline(__always)
@@ -42,16 +34,6 @@ public struct Gimli {
 
 private extension SIMD4<UInt32> {
     @inline(__always)
-    init(littleEndian value: Self) {
-        self.init(
-            Scalar(littleEndian: value.x),
-            Scalar(littleEndian: value.y),
-            Scalar(littleEndian: value.z),
-            Scalar(littleEndian: value.w)
-        )
-    }
-    
-    @inline(__always)
     func rotated(left count: Scalar) -> Self {
         (self &<< count) | (self &>> (32 - count))
     }
@@ -60,14 +42,8 @@ private extension SIMD4<UInt32> {
     subscript(_ v0: Scalar, _ v1: Scalar, _ v2: Scalar, _ v3: Scalar) -> Self {
         self[Self(v0, v1, v2, v3)]
     }
-    
-    @inline(__always)
-    var littleEndian: Self {
-        Self(
-            x.littleEndian,
-            y.littleEndian,
-            z.littleEndian,
-            w.littleEndian
-        )
-    }
 }
+
+#if _endian(big)
+#error("Big-endian platforms are currently not supported")
+#endif
